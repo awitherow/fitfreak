0ae7376b7cb9bbbd1ed6c262251159dafcd947ed
awitherow <au.witherow@gmail.com>
2016-09-02 13:33:08 +0200

    adds more commands

--- commands/messages.js
+++ commands/messages.js
@@ -1,6 +1,9 @@
 var messages = [
-    ':muscle: Get up, do some push ups or face pulls. Afterwards, drink some water! :potable_water:',
-    'when is the last time you moved? sweat and replenish! #fitfam #fuckyeahfitness :muscle:',
+    ':muscle: Get up and move. Afterwards, drink some water! :potable_water:',
+    'When is the last time you moved? Sweat and replenish! #fuckyeahfitness :muscle:',
+    'CHALLENGE! Do as many pushups as you can in a row, right now! :muscle:',
+    'CHALLENGE! Do as many facepulls (kettlebell) as you can in a row, right now! :muscle:',
+    'work on that fine ass, walk down the stairs to the first floor, and back up!'
 ];
 
 module.exports = messages;
