700b0e3ffc407f1117cad85fef36e2a3d377e0a1
Austin Witherow <au.witherow@gmail.com>
2016-10-05 22:05:03 +0200

    relocates bot in babel

--- index.js
+++ index.js
@@ -1,25 +1,2 @@
-require('dotenv').config();
-var moment = require('moment');
-var Bot = require('slackbots');
-var messages = require('./commands/messages');
-
-var bot = new Bot({
-    token: process.env.SLACK_BOT_TOKEN,
-    name: 'fitfreak'
-});
-
-var hour = 60*60*1000;
-
-bot.on('start', function() {
-    movePiggy();
-    setInterval(movePiggy, hour);
-});
-
-function movePiggy() {
-    var message = messages[Math.floor(Math.random()*messages.length)];
-    var users = ['austin'];
-    users.forEach(user => {
-        bot.postMessageToUser(user, message);
-        console.log('message sent to ' + user + ' at ' + moment().calendar());
-    });
-}
+require("babel-core").transform("code");
+require('./src/fitfreak.js');
---
+++ src/fitfreak.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+import moment from 'moment'
+import Bot from 'slackbots'
+import messages from './commands/messages'
+import visitThePostOffice from './post-office'
+
+const bot = new Bot({
+    token: process.env.SLACK_BOT_TOKEN,
+    name: 'fitfreak'
+})
+
+const hour = 60*60*1000
+
+bot.on('message', function(data) {
+    visitThePostOffice(bot, data)
+})
+
+bot.on('start', function() {
+    //movePiggy()
+    //setInterval(movePiggy, hour)
+})
+
+function movePiggy() {
+    var message = messages[Math.floor(Math.random()*messages.length)]
+    var users = ['austin']
+    users.forEach(user => {
+        bot.postMessageToUser(user, message)
+        console.log('message sent to ' + user + ' at ' + moment().calendar())
+    })
+}
