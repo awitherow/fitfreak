ac7891b05bf96a51af5696bc0b8fd09050a86e10
awitherow <au.witherow@gmail.com>
2016-10-07 16:42:19 +0200

    console.logs the senders data from google spreadsheet

--- src/post-office.js
+++ src/post-office.js
@@ -22,10 +22,26 @@ function sendToMessageProcessing(data) {
   const sender = users._value.members.filter((user) => user.id === data.user)[0]
   if (sender) {
     spreadsheetConnect(auth => {
-      let userData = getUserData(auth, sender.name)
-      bot.postMessageToUser(sender.name, 'Hello')
+      let userData = getUserData(auth, sender.name, function(err, res) {
+        res = res[0]; // single user
+        if (!err && err === 'no data found') {
+          // create user row
+        } else {
+          const userDataColumns = config.userDataColumns;
+          bot.postMessageToUser(sender.name, `Hello ${sender.name}`)
+          let data = {};
+          for (let i = 0; i < userDataColumns.length; i++) {
+            const col = userDataColumns[i]
+            const key = col.name
+            const type = col.type
+            data[key] = handleData(type, res[i])
+          }
+          console.log(data)
+        }
+      });
     });
   }
+}
 
 function handleData(type, data) {
   switch(type) {
