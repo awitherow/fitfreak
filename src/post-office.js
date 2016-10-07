cb2318af322f75a0277eeacba8c7a29a818f5ab4
awitherow <au.witherow@gmail.com>
2016-10-07 16:41:54 +0200

    creates data type handler

--- src/post-office.js
+++ src/post-office.js
@@ -27,4 +27,11 @@ function sendToMessageProcessing(data) {
     });
   }
 
+function handleData(type, data) {
+  switch(type) {
+    case 'string': return data;
+    case 'json': return JSON.parse(data)
+    case 'number': return parseInt(data)
+    default: return data
+  }
 }
