7906ffed8d8d13edde94fa957250ec8b03b6c6d0
awitherow <au.witherow@gmail.com>
2016-10-07 16:13:15 +0200

    eslint

--- src/auth.js
+++ src/auth.js
@@ -1,20 +1,20 @@
-var fs = require('fs');
-var readline = require('readline');
-var google = require('googleapis');
-var googleAuth = require('google-auth-library');
+import fs from 'fs'
+import readline from 'readline'
+import google from 'googleapis'
+import googleAuth from 'google-auth-library'
 
-var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+const TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json'
 
 function spreadsheetConnect(cb) {
-  fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  fs.readFile('client_secret.json', function(err, content) {
     if (err) {
-      console.log('Error loading client secret file: ' + err);
-      return;
+      console.log('Error loading client secret file: ' + err)
+      return
     }
-    authorize(JSON.parse(content), cb);
+    authorize(JSON.parse(content), cb)
   });
 }
 
@@ -23,42 +23,42 @@ export {
 }
 
 function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+  const clientSecret = credentials.installed.client_secret
+  const clientId = credentials.installed.client_id
+  const redirectUrl = credentials.installed.redirect_uris[0]
+  const auth = new googleAuth()
+  const oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl)
 
   fs.readFile(TOKEN_PATH, function(err, token) {
     if (err) {
-      getNewToken(oauth2Client, callback);
+      getNewToken(oauth2Client, callback)
     } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
+      oauth2Client.credentials = JSON.parse(token)
+      callback(oauth2Client)
     }
   });
 }
 
 function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
+  const authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
+  })
+  console.log('Authorize this app by visiting this url: ', authUrl)
+  const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+  })
   rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
+    rl.close()
     oauth2Client.getToken(code, function(err, token) {
       if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
+        console.log('Error while trying to retrieve access token', err)
+        return
       }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
+      oauth2Client.credentials = token
+      storeToken(token)
+      callback(oauth2Client)
     });
   });
 }
--- src/spreadsheets.js
+++ src/spreadsheets.js
@@ -1,22 +1,21 @@
-var google = require('googleapis');
+import google from 'googleapis'
 
-function getUserData(auth, name) {
-  var sheets = google.sheets('v4');
+function getUserData(auth, name, cb) {
+  var sheets = google.sheets('v4')
   sheets.spreadsheets.values.get({
     auth: auth,
     spreadsheetId: '15y8thmW_8rWHzmO4O5aJ097M7VqPVcyGIE6yoQxfT9Q',
     range: 'User Data!A1:D',
-  }, function(err, response) {
+  }, function(err, res) {
     if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
+      console.log('The API returned an error: ' + err)
+      return
     }
-    var rows = response.values;
+    const rows = res.values
     if (rows.length == 0) {
-      console.log('No data found.');
+      cb('no data found', {})
     } else {
-      console.log(rows);
-      return rows;
+      cb(null, rows.filter(row => row[0] === name))
     }
   });
 }
